ead6664c29c7647f583830a722860934eaadedd1
lutaoact <i@lutao.me>
2013-10-27 15:41:37 +0800

    express_server.js: process different path

--- express_server.js
+++ express_server.js
@@ -8,4 +8,19 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
+app.get('/test', function(req, res) {
+    res.send('test render');
+});
+
+app.get('/user/:id', function(req, res) {
+    res.send('user: ' + req.params.id + ' user page');
+});
+
+app.get(/^\/ip?(?:\/(\d{2,3})(?:\.(\d{2,3}))(?:\.(\d{2,3}))(?:\.(\d{2,3})))?/, function(req, res) {
+    res.send(req.params);
+});
+
+app.get('*', function(req, res) {
+    res.send('Page not found', 404);
+});
 console.log("express server running at http://127.0.0.1:" + port);
