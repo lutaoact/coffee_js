8acfc57d47363b168f5b507ddbd09cc1ff051cb5
lutaoact <i@lutao.me>
2013-10-27 16:56:55 +0800

    form_POST.js

---
+++ form_POST.js
@@ -0,0 +1,36 @@
+var http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    qs = require('querystring'),
+    server;
+
+server = http.createServer(function(req, res) {
+    var urlData,
+        encode = 'utf8',
+        filePath = 'static/express_post_example_form.html',
+        formData,
+        action;
+    urlData = url.parse(req.url, true);
+    action = urlData.pathname;
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+
+    if (action === '/Signup') {
+        formData = '';
+        req.on('data', function(data) {
+            formData += data;
+        });
+
+        req.on('end', function() {
+            user = qs.parse(formData);
+            res.end('<h1>' + user.username + ' welcome</h1><p>我們已經將會員啟用信寄至' + user.email + "</p>");
+        });
+    } else {
+        fs.readFile(filePath, encode, function(err, file) {
+            res.write(file);
+            res.end();
+        });
+    }
+});
+
+server.listen(3000);
+console.log('Server is running. The time is: ' + new Date());
