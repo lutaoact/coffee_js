2c43244d7c92da556021cc4f2318183a1134b520
lutaoact <i@lutao.me>
2013-11-01 14:01:03 +0800

    modify server.js: use different domain

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var port = 4080;
 var server = require('./basicServer').createServer();
 server.useFavicon('localhost', './docroot/favicon.ico');
 server.docroot('localhost', '/', './docroot');
+server.useFavicon('example.com', './docroot/favicon.ico');
+server.docroot('example.com', '/', './docroot');
 require('./httpsniffer').sniffOn(server);
 server.listen(port);
