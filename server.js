19ec10d8bb94e6baa973dea8f009ffa5e17936fa
lutaoact <i@lutao.me>
2013-11-01 14:44:45 +0800

    undone: redirector.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var port = 4080;
 var server = require('./basicServer').createServer();
+//server.addContainer('.*', '/l/(.*)$', require('./redirector'), { });
 server.useFavicon('localhost', './docroot/favicon.ico');
 server.docroot('localhost', '/', './docroot');
 server.useFavicon('example.com', './docroot/favicon.ico');
