28bf621310b8060cd9cfe913a5d1bfd793f6276e
lutaoact <i@lutao.me>
2014-06-27 16:06:30 +0800

    byteLength.js

--- byteLength.js
+++ byteLength.js
@@ -1,6 +1,51 @@
-str = '\u00bd + \u00bc = \u00be';
+var str = '\u00bd + \u00bc = \u00be';
 
 console.log(str + ": " + str.length + " characters, " +
           Buffer.byteLength(str, 'utf8') + " bytes");
 
 // ½ + ¼ = ¾: 9 characters, 12 bytes
+var buf = new Buffer(256);
+var len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+str = "node.js";
+buf = new Buffer(str.length);
+
+for (var i = 0; i < str.length ; i++) {
+      buf[i] = str.charCodeAt(i);
+}
+console.log(buf);
+
+var buf = new Buffer(4);
+
+buf[0] = 0x3;
+buf[1] = 0x4;
+buf[2] = 0x23;
+buf[3] = 0x42;
+
+//for (var ii = 0; ii < buf.length; ii++) {
+//    console.log(buf.readUInt8(ii));
+//}
+
+var buf = new Buffer(4);
+
+buf[0] = 0x3;
+buf[1] = 0x4;
+buf[2] = 0x23;
+buf[3] = 0x42;
+
+console.log(buf.readUInt16BE(0));
+console.log(buf.readUInt16LE(0));
+console.log(buf.readUInt16BE(1));
+console.log(buf.readUInt16LE(1));
+console.log(buf.readUInt16BE(2));
+console.log(buf.readUInt16LE(2));
+
+var buf = new Buffer(4);
+
+buf[0] = 0x00;
+buf[1] = 0x00;
+buf[2] = 0x80;
+buf[3] = 0x3f;
+
+console.log(buf.readFloatLE(0));
