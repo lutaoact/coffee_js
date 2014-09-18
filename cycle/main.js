81eb563dbbf1f7cf3d3e7738c196bfb63cca6ae1
lutaoact <i@lutao.me>
2014-09-18 18:26:42 +0800

    understand cycle in nodejs require

--- cycle/main.js
+++ cycle/main.js
@@ -1,3 +1,5 @@
+//在require循环调用中，当require函数返回时
+//有可能这个文件还没有被执行完
 console.log('main starting');
 var a = require('./a.js');
 var b = require('./b.js');
@@ -9,4 +11,5 @@ console.log('in main, a.done = %j, b.done = %j', a.done, b.done);
  * an unfinished copy of the a.js exports object is returned to the b.js module.
  * b.js then finishes loading,
  * and its exports object is provided to the a.js module.
+ * 如果a.js的exports对象不返回，则会陷入无限循环
  */
