fc1b958a932ca2d8b7b15c50cae3da72bba6386c
lutaoact <i@lutao.me>
2014-09-21 00:02:05 +0800

    setSlaveOk

--- mongo_replica_set.js
+++ mongo_replica_set.js
@@ -1,7 +1,11 @@
 replicaSet = new ReplSetTest({nodes: 3});
 replicaSet.startSet(); replicaSet.initiate();
-db.getMongo().setSlaveOK();
+db.getMongo().setSlaveOk();
 
 conn1 = new Mongo("localhost:31000");
 primaryDB = conn1.getDB("test");
 primaryDB.isMaster();
+
+//> secondary.coll.find()
+//  error: { "$err" : "not master and slaveOk=false", "code" : 13435 }
+//> secondary.getMongo().setSlaveOk();
