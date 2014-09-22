a9aaff2c86abd84c94a4cba0fc86ed9776780e80
lutaoact <i@lutao.me>
2014-09-22 10:33:50 +0800

    local.system.replset

--- mongo_replica_set.js
+++ mongo_replica_set.js
@@ -9,3 +9,5 @@ primaryDB.isMaster();
 //> secondary.coll.find()
 //  error: { "$err" : "not master and slaveOk=false", "code" : 13435 }
 //> secondary.getMongo().setSlaveOk();
+
+//db.system.replset.find() #副本集配置。副本集中所有成员的这个文档都是相同的，绝对不要用update更新这个文档
