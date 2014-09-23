85011bc47c2287619c1b18d32e99914bf9bdc5fb
lutaoact <i@lutao.me>
2014-09-23 17:05:08 +0800

    rs.stepDown() & rs.freeze()

--- mongo_replica_set.js
+++ mongo_replica_set.js
@@ -11,3 +11,6 @@ primaryDB.isMaster();
 //> secondary.getMongo().setSlaveOk();
 
 //db.system.replset.find() #副本集配置。副本集中所有成员的这个文档都是相同的，绝对不要用update更新这个文档
+
+rs.stepDown(time) //让主节点退化为备份节点，以秒为单位，默认60s
+rs.freeze(time) //保持备份节点状态，以秒为单位
