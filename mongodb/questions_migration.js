be6604529a944f87c7a118811a3714121d5e2459
lutaoact <i@lutao.me>
2014-10-09 18:38:16 +0800

    mongodb/questions_migration.js

---
+++ mongodb/questions_migration.js
@@ -0,0 +1,24 @@
+var result = db.questions.update({}, {
+  $rename: {
+    "content.title"   : "body",
+    "content.body"    : "options",
+    "solution"        : 'detailSolution',
+  },
+}, {multi: true});
+
+printjson(result);
+
+result = db.questions.update({}, {$unset: {content: ''}}, {multi: true});
+
+printjson(result);
+
+db.questions.find({}).forEach(function(q) {
+  print(q._id);
+  newOptions = q.options.map(function(option) {
+    return {
+      choice: option.desc,
+      correct: option.correct,
+    };
+  });
+  db.questions.update({_id: q._id}, {$set: {options: newOptions}});
+});
