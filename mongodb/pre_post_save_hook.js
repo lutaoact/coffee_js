fdb044501023830e98aa45a5e0c084d119d60fe3
lutaoact <i@lutao.me>
2014-11-12 16:49:49 +0800

    pre_post_save_hook.js: console.log(this.isNew)

--- mongodb/pre_post_save_hook.js
+++ mongodb/pre_post_save_hook.js
@@ -4,8 +4,16 @@ var connection = mongoose.connect('mongodb://localhost/ft').connection;
 var Schema = mongoose.Schema;
 var schema = new Schema({name: String, value: Number});
 
+schema.pre('save', function (next) {
+  console.log(this.isNew);//新文档，这个值为true
+  console.log('this fired before a document was saved');
+  next();
+});
+
 schema.post('save', function (doc) {
   console.log('this fired after a document was saved');
+  console.log(this.isNew);//保存之后，这个值始终为false
+  console.log(doc);
 });
 
 var Model = mongoose.model('model', schema);
@@ -13,4 +21,10 @@ var Model = mongoose.model('model', schema);
 var m = new Model({name: 'hello', value: 10});
 m.save(function (err) {
   console.log('this fired after the `post` hook');
+  Model.findOne({name: 'hello'}, function(err, doc) {
+    doc.name = 'good'
+    doc.save(function(err) {
+      console.log('run finally');
+    });
+  });
 });
