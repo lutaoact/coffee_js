9c0fd1929ab94ac504ec761b5464f624e62070bc
lutaoact <i@lutao.me>
2015-07-09 17:30:32 +0800

    emitter.js

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@ emitter.on('event1', function(func) {
       func('no err', 'hello world');
     });//异步执行
 });
-emitter.emit('event1', console.log);
-
 emitter.on('event2', console.log);
+
+emitter.emit('event1', console.log);
 emitter.emit('event2', 'hello world');
