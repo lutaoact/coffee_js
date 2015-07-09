74b52f0388a11f1a11ffa0de9bacfa35c209277e
lutaoact <i@lutao.me>
2015-07-09 18:19:27 +0800

    currying.js

--- currying.js
+++ currying.js
@@ -1,14 +1,17 @@
+//在计算机科学中，柯里化（Currying），是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
 var accu = 0, count = 10;
 for (var i = 0; i < count; i++) {
-    setTimeout(
-        function(i) {
-            console.log(i);
-            return function() {
-                count--;
-                accu += i;
-                console.log(accu);
-            };
-        }(i),
-        1
-    );
+  setImmediate((function(i) {
+    return function() {
+      accu += i;
+      console.log(accu);
+    };
+  })(i));
 }
+
+var foo = function(a) {
+  return function(b) {
+    return a * a + b * b;
+  };
+};
+console.log(foo(3)(4));
