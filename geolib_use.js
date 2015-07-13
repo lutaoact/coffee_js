bf61b4432381974d0c8ff55dbece62015ae54379
lutaoact <i@lutao.me>
2015-07-13 12:17:59 +0800

    geolib_use.js

---
+++ geolib_use.js
@@ -0,0 +1,5 @@
+var geolib = require('geolib');
+console.log(geolib.sexagesimal2decimal("51° 29' 46\" N"));
+console.log(geolib.sexagesimal2decimal("51° 29' 46\" S"));
+console.log(geolib.sexagesimal2decimal("51° 29' 46\" E"));
+console.log(geolib.sexagesimal2decimal("51° 29' 46\" W"));
