33a3c5185784ad099a311fa4205645e5f6654e1f
lutaoact <i@lutao.me>
2015-07-15 17:03:52 +0800

    pinyin_use.js

---
+++ pinyin_use.js
@@ -0,0 +1,25 @@
+var pinyin = require('pinyin');
+
+//console.log(pinyin("中心"));    // [ [ 'zhōng' ], [ 'xīn' ] ]
+//console.log(pinyin("中心", {
+//  heteronym: true               // 启用多音字模式
+//}));                            // [ [ 'zhōng', 'zhòng' ], [ 'xīn' ] ]
+//console.log(pinyin("中心", {
+//  heteronym: true,              // 启用多音字模式
+//  segment: true                 // 启用分词，以解决多音字问题。
+//}));                            // [ [ 'zhōng' ], [ 'xīn' ] ]
+//console.log(pinyin("中心", {
+//  style: pinyin.STYLE_INITIALS, // 设置拼音风格
+//  heteronym: true
+//})); 
+
+console.log(pinyin("长沙", {
+  style: pinyin.STYLE_FIRST_LETTER, // 设置拼音风格
+  heteronym: true,
+})); 
+
+console.log(pinyin("长沙", {
+  style: pinyin.STYLE_FIRST_LETTER, // 设置拼音风格
+  heteronym: true,
+  segment: true                 // 启用分词，以解决多音字问题。
+})); 
