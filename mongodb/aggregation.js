b5bad624704e05f610367099eff479ecd6481eab
lutaoact <i@lutao.me>
2015-09-15 18:35:37 +0800

    mongo aggregate $group

--- mongodb/aggregation.js
+++ mongodb/aggregation.js
@@ -15,7 +15,7 @@ db.orders.mapReduce(
 //The $match and $sort pipeline operators can take advantage of an index when they occur at the beginning of the pipeline.
 
 /**
-如果group操作针对的不是已经存在的文档字段，而是计算得出的，则用$keyf关键字代替key
+如果group操作针对的不是已经存在的文档字段，而是运算得出的，则用$keyf关键字代替key
 Use $keyf instead of key to group by calculated fields rather than existing document fields.
 {
   group:
@@ -70,4 +70,55 @@ db.runCommand(
                    }
        }
    }
-)
+);
+
+/** 操作符列表：
+* $project 重塑每个文档，按需求增删字段。每一个输入文档，产生一个输出文档
+* $match 使用标准的mongoDB查询
+* $let 可以构建临时变量，只在$let中有效
+* $map 输入和输出都是一个数组
+* $redact Restricts the contents of the documents based on information stored in the documents themselves.（水太深，搞不太明白）
+  基于文档自身存储的信息来限制文档内容 $$DESCEND $$PRUNE $$KEEP
+* $limit 确定最大的文档传递个数。
+* $unwind 解构数组字段，并输出文档，包含所有字段，并会用数组的一个元素来代替之前的数组，所以一个输入文档，可能会产生多个输出文档
+* $group 注意与group命令和collection的group快捷操作区分开
+*/
+var fenFactor = 100;
+db.sales.aggregate( [
+   {
+      $project: {
+         finalTotal: {
+            $let: {
+               vars: {
+                  total: { $add: [ '$price', '$tax' ] },
+                  discounted: { $cond: { if: '$applyDiscount', then: 0.9, else: 1 } },
+                  fenFactor: '$$fenFactor'
+               },
+               in: { $multiply: [ "$$total", "$$discounted", "$$fenFactor" ] }
+            }
+         }
+      }
+   }
+] );
+
+db.sales.aggregate( [
+   {
+      $project: {
+         finalTotal: {
+          $multiply: [{
+             $add: [ '$price', '$tax' ],
+          }, {
+             $cond: { if: '$applyDiscount', then: 0.9, else: 1 }
+          }],
+         }
+      }
+   }
+] );
+/** $group
+_id为必须字段，但是可以将_id设为null来计算所有输入文档的累加值
+The _id field is mandatory; however, you can specify an _id value of null to calculate accumulated values for all the input documents as a whole.
+$sum 会忽略非数字值
+$avg 会忽略非数字值
+$first Only meaningful when documents are in a defined order. 只在排序时有意义
+$last Only meaningful when documents are in a defined order. 只在排序时有意义
+*/
