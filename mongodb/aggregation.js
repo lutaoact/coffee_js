74a5d45fd90ffed7cc08d64c30356b3a972ef8e1
lutaoact <i@lutao.me>
2015-09-15 15:59:30 +0800

    aggregation: group

--- mongodb/aggregation.js
+++ mongodb/aggregation.js
@@ -31,3 +31,43 @@ Use $keyf instead of key to group by calculated fields rather than existing docu
 //在mongo shell中，有一个包装方法提供快捷操作，接受的参数是keyf和reduce，而在执行group命令的时候，用的是$keyf和$reduce字段。（真是受不了……）
 For the shell, MongoDB provides a wrapper method db.collection.group(). However, the db.collection.group() method takes the keyf field and the reduce field whereas the group command takes the $keyf field and the $reduce field.
 */
+/** orders的数据结构
+{
+  _id: ObjectId("5085a95c8fada716c89d0021"),
+  ord_dt: ISODate("2012-07-01T04:00:00Z"),
+  ship_dt: ISODate("2012-07-02T04:00:00Z"),
+  item:
+    {
+      sku: "abc123",
+      price: 1.99,
+      uom: "pcs",
+      qty: 25
+    }
+}
+*/
+db.runCommand(
+   {
+     group:
+       {
+         ns: 'orders',
+         $keyf: function(doc) {
+                    return { day_of_week: doc.ord_dt.getDay() };
+                },
+         cond: { ord_dt: { $gt: new Date( '01/01/2012' ) } },
+         $reduce: function( curr, result ) {
+                      result.total += curr.item.qty;
+                      result.count++;
+                  },
+         initial: { total : 0, count: 0 },
+         finalize: function(result) {
+                      var weekdays = [
+                           "Sunday", "Monday", "Tuesday",
+                           "Wednesday", "Thursday",
+                           "Friday", "Saturday"
+                          ];
+                      result.day_of_week = weekdays[result.day_of_week];
+                      result.avg = Math.round(result.total / result.count);
+                   }
+       }
+   }
+)
