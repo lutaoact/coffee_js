9e05a27dea5de78560465cdd0bb6dcc253501e38
lutaoact <i@lutao.me>
2015-10-12 17:01:03 +0800

    aggregate_order_by_manicurist.js

---
+++ mongodb/aggregate_order_by_manicurist.js
@@ -0,0 +1,23 @@
+db.orders.aggregate([{
+  $match: {
+    manicurist: '0210001',
+  },
+}, {
+  $group: {
+    _id: '$cellphone',
+    count: {$sum: 1},
+    totalRealPrice: {$sum: '$real_price'},
+  },
+}, {
+  $project: {
+    _id: 0,
+    cellphone: '$_id',
+    count: 1,
+    totalRealPrice: 1,
+    avgRealPrice: {$divide: ['$totalRealPrice', '$count']},
+  },
+}, {
+  $sort: {count: -1},
+}, {
+  $limit: 15
+}]);
