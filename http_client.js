93c4d480b32fa56450701825f32cce2a84fa0b5a
lutaoact <i@lutao.me>
2016-02-01 11:30:03 +0800

    http_client.js http_server.js

---
+++ http_client.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const http = require('http');
+
+let options = {
+  hostname: '127.0.0.1',
+  port: 1337,
+  path: '/',
+  method: 'GET',
+};
+
+let req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(chunk);
+  });
+});
+
+req.on('error', (e) => {
+  console.log(`problem with request: ${e.message}`);
+});
+
+req.end();
