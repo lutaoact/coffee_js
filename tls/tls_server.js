5d85487321d9bbd5ed7d98dde746c0513e3afa2b
lutaoact <i@lutao.me>
2016-02-02 10:37:07 +0800

    tls_server.js

---
+++ tls/tls_server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const tls = require('tls');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.crt'),
+  requestCert: true,
+  ca: [fs.readFileSync('./ca.crt')]
+};
+
+const server = tls.createServer(options, (socket) => {
+  console.log(`server connected ${socket.authorized ? 'authorized' : 'unauthorized'}`);
+  socket.setEncoding('utf8');
+  socket.pipe(socket);
+});
+
+server.listen(8000, () => {
+  console.log('tls server listenning on 127.0.0.1:8000...');
+});
+
+//openssl s_client -connect 127.0.0.1:8000
