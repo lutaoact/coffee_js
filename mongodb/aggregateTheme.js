b9695e409dc1911a6940984f68a01597b66ebf7b
lutaoact <i@lutao.me>
2016-07-19 14:33:58 +0800

    aggregateTheme.js: completed

--- mongodb/aggregateTheme.js
+++ mongodb/aggregateTheme.js
@@ -7,9 +7,37 @@ db.theme.aggregate([{
   }
 }]);
 
+//只包含code
 db.theme_stock.aggregate([{
   $group: {
     _id: {$substr: ['$tcode', 2, 6]},
     stocks: {$push: {$substr: ['$scode', 2, 6]}},
   }
+}, {
+  $out: 'tmp_aggregate_theme'
+}]);
+
+db.theme_stock.aggregate([{
+  $group: {
+    _id: '$tcode',
+    stocks: {$push: {$substr: ['$scode', 2, 6]}},
+  }
+}, {
+  $lookup: {
+    from: 'theme',
+    localField: '_id',
+    foreignField: 'iCode',
+    as: 'theme',
+  }
+}, {
+  $unwind: '$theme'
+}, {
+  $project: {
+    _id: false,
+    code: {$substr: ['$_id', 2, 6]},
+    name: '$theme.name',
+    stocks: true,
+  }
+}, {
+  $out: 'tmp_aggregate_theme'
 }]);
