92c4f517c2bb186ae6722e20708e88cc342ebcc9
lutaoact <i@lutao.me>
2016-11-17 16:14:32 +0800

    net_client.js

--- net_client.js
+++ net_client.js
@@ -1,9 +1,9 @@
 var net = require('net');
 
 //var client = net.connect({path: '/tmp/echo.sock'}, () => {
-var client = net.connect({port: 8124}, () => {//connection listener
+var client = net.connect({host: '127.0.0.1', port: 9124}, () => {//connection listener
   console.log('client connected');
-  client.write('world!\n');
+  client.write('{"hello":"world"}');
   client.on('data', (data) => {
     console.log(data.toString());
     client.end();
