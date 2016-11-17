3a45f5924f535800bd85d86c8c11d6b7679062a7
lutaoact <i@lutao.me>
2016-11-17 16:10:08 +0800

    ws_client.js

--- ws_client.js
+++ ws_client.js
@@ -1,5 +1,6 @@
 var WebSocket = require('faye-websocket'),
-    ws        = new WebSocket.Client('ws://localhost:9000/sockjs');
+//    ws        = new WebSocket.Client('ws://localhost:1111/ws?token=xxxxx');
+    ws        = new WebSocket.Client('ws://dev.api.gw.stockalert.cn/ws?token=xxxx');
 
 ws.on('open', function(event) {
     console.log('open');
@@ -10,10 +11,11 @@ ws.on('open', function(event) {
 });
 
 ws.on('message', function(event) {
-    console.log(event.data);
+  console.log(event.data);
 });
 
 ws.on('close', function(event) {
+  console.log('close', event);
   console.log('close', event.code, event.reason);
   ws = null;
 });
