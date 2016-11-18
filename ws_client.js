8892c2f2aba1ea7dc8aae6c0374df9994fa917bd
lutaoact <i@lutao.me>
2016-11-18 13:57:34 +0800

    ws_client.js

--- ws_client.js
+++ ws_client.js
@@ -1,10 +1,13 @@
-var WebSocket = require('faye-websocket'),
-//    ws        = new WebSocket.Client('ws://localhost:1111/ws?token=xxxxx');
-    ws        = new WebSocket.Client('ws://dev.api.gw.stockalert.cn/ws?token=xxxx');
+'use strict';
 
+var WebSocket = require('faye-websocket'),
+    ws        = new WebSocket.Client('ws://localhost:1111/ws?did=2');
+//    ws        = new WebSocket.Client('ws://dev.api.gw.stockalert.cn/ws?token=xxxx');
+
+let interval = null;
 ws.on('open', function(event) {
     console.log('open');
-    setInterval(function() {
+    interval = setInterval(function() {
 //        if (ws) ws.send(JSON.stringify({user_id: process.env.U}));
         ws.send('hello world');
     }, 1000);
@@ -15,7 +18,7 @@ ws.on('message', function(event) {
 });
 
 ws.on('close', function(event) {
-  console.log('close', event);
   console.log('close', event.code, event.reason);
+  clearInterval(interval);
   ws = null;
 });
